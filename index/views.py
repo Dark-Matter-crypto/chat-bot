6b475c164c8051113843db35efa643f8f4e02ac8
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2021-01-15 18:14:07 +0200

    Added model functions

--- index/views.py
+++ index/views.py
@@ -3,6 +3,7 @@ import numpy
 import nltk
 import random
 import json
+import pickle
 # import tensorflow
 # import tflearn
 from nltk.stem.lancaster import LancasterStemmer
@@ -10,8 +11,7 @@ stemmer = LancasterStemmer()
 from django.shortcuts import render, HttpResponse
 
 
-def load_data_file():
-    module_dir = os.path.dirname(__file__)
+def load_intents_file(module_dir):
     intents_file_path = os.path.join(module_dir, 'static\index\data\intents.json')
     with open(intents_file_path) as intents_file:
         data = json.load(intents_file)
@@ -19,57 +19,101 @@ def load_data_file():
     return data
 
 
+def save_data_file(module_dir, words, tags, training, output):
+    data_file_path = os.path.join(module_dir, 'static\index\data\data.pickle')
+    with open(data_file_path, "wb") as data_file:
+        pickle.dump((words, tags, training, output), data_file)
+
+
+def save_model(module_dir, model):
+    model_path = os.path.join(module_dir, 'static\index\model\model.tflearn')
+    model.save(model_path)
+
+
+def open_model(module_dir, model):
+    model_path = os.path.join(module_dir, 'static\index\model\model.tflearn')
+    model.load(model_path)
+    return model
+
+
+def setup_model(training, output):
+    tensorflow.reset_default_graph()
+
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
+
+
 # Create your views here.
 
 def index_view(request):
-    data = load_data_file()
+    module_dir = os.path.dirname(__file__)
 
-    words = []
-    tags = []
-    patterns = []
-    pattern_tags = []
+    try:
+        data_file_path = os.path.join(module_dir, 'static\index\data\data.pickle')
+        with open(data_file_path, "rb") as data_file:
+            words, tags, training, output = pickle.load(data_file)
 
-    for intent in data['intents']:
-        for pattern in intent['patterns']:
-            pattern_words = nltk.word_tokenize(pattern)
-            words.extend(pattern_words)
-            patterns.append(pattern_words)
-            pattern_tags.append(intent['tag'])
+    except FileNotFoundError:
+        data = load_intents_file(module_dir)
+        words = []
+        tags = []
+        patterns = []
+        pattern_tags = []
 
-        if intent['tag'] not in tags:
-            tags.append(intent['tag'])
+        for intent in data['intents']:
+            for pattern in intent['patterns']:
+                pattern_words = nltk.word_tokenize(pattern)
+                words.extend(pattern_words)
+                patterns.append(pattern_words)
+                pattern_tags.append(intent['tag'])
 
-    words = [stemmer.stem(word.lower()) for word in words if w != "?"]
-    words = sorted(list(set(words)))
+            if intent['tag'] not in tags:
+                tags.append(intent['tag'])
 
-    tags = sorted(tags)
+        words = [stemmer.stem(word.lower()) for word in words if w != "?"]
+        words = sorted(list(set(words)))
 
-    training = []
-    output = []
+        tags = sorted(tags)
 
-    out_empty = [0 for _ in range(len(tags))]
+        training = []
+        output = []
 
-    for p, pattern in enumerate(patterns):
-        bag = []
-        pattern_words = [stemmer.stem(word) for word in pattern]
+        out_empty = [0 for _ in range(len(tags))]
 
-        for word in words:
-            if word in pattern_words:
-                bag.append(1)
-            else:
-                bag.append(0)
+        for p, pattern in enumerate(patterns):
+            bag = []
+            pattern_words = [stemmer.stem(word) for word in pattern]
 
-        output_row = out_empty[:]
-        output_row[tags.index(pattern_tags[p])] = 1
+            for word in words:
+                if word in pattern_words:
+                    bag.append(1)
+                else:
+                    bag.append(0)
 
-        training.append(bag)
-        output.append(output_row)
+            output_row = out_empty[:]
+            output_row[tags.index(pattern_tags[p])] = 1
 
-    training = numpy.array(training)
-    output = numpy.array(output)
+            training.append(bag)
+            output.append(output_row)
 
+        training = numpy.array(training)
+        output = numpy.array(output)
 
+        save_data_file(module_dir, words, tags, training, output)
 
+    model = setup_model(training, output)
 
-
+    try:
+        open_model(module_dir, model)
+    except FileNotFoundError:
+        model.fit(training, output, n_epoch=5000, batch_size=8, show_metric=True)
+        save_model(module_dir, model)
+        
     return HttpResponse(tags)

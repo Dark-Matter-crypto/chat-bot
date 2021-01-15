d02e2eed200f472785fe6ff095ae2c02c5567223
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2021-01-15 20:52:28 +0200

    Added a model training function

--- index/views.py
+++ index/views.py
@@ -25,17 +25,6 @@ def save_data_file(module_dir, words, tags, training, output):
         pickle.dump((words, tags, training, output), data_file)
 
 
-def save_model(module_dir, model):
-    model_path = os.path.join(module_dir, 'static\index\model\model.tflearn')
-    model.save(model_path)
-
-
-def open_model(module_dir, model):
-    model_path = os.path.join(module_dir, 'static\index\model\model.tflearn')
-    model.load(model_path)
-    return model
-
-
 def setup_model(training, output):
     tensorflow.reset_default_graph()
 
@@ -48,6 +37,20 @@ def setup_model(training, output):
     net = tflearn.regression(net)
 
     model = tflearn.DNN(net)
+    return model
+
+
+def user_bag_of_words(user_input, words):
+    bag = [0 for _ in range(len(words))]
+    user_words = nltk.word_tokenize(user_input)
+    user_words = [stemmer.stem(word.lower()) for word in user_words]
+
+    for word in user_words:
+        for i, w in enumerate(user_input):
+            if w == word:
+                bag[i] = 1
+
+    return numpy.array(bag)
 
 
 # Create your views here.
@@ -110,10 +113,32 @@ def index_view(request):
 
     model = setup_model(training, output)
 
+    model_path = os.path.join(module_dir, 'static\index\model\model.tflearn')
+
     try:
-        open_model(module_dir, model)
-    except FileNotFoundError:
+        model.load(model_path)
+    except:
         model.fit(training, output, n_epoch=5000, batch_size=8, show_metric=True)
-        save_model(module_dir, model)
-        
-    return HttpResponse(tags)
+        model.save(model_path)
+
+    unknown_query_response = [
+        "Hmmm...Not sure what you asking there.",
+        "This is me telling you I didn't understand what you just said.",
+        "Not sure what you asking there. Sorry.",
+        "Didn't catch that. Please try another question.",
+        "Sorry, didn't understand your question.",
+        "Escuze moire?"
+    ]
+    results = model.predict([user_bag_of_words(user_input, words)])[0]
+    results_index = numpy.argmax(results)
+    tag = tags[results_index]
+
+    if results[results_index] > 0.85:
+        for intent in data['intents']:
+            if intent['tag'] == tag:
+                responses = intent['responses']
+        response = random.choice(responses)
+    else:
+        response = random.choice(unknown_query_response)
+
+    return HttpResponse(response)

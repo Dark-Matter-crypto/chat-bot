ccec93d69a1d57d179c58ac2c9216692a20a63fd
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2021-01-15 16:44:38 +0200

    Created UserQuery model

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserQuery
 
 # Register your models here.
+
+class UserQueryAdmin(admin.ModelAdmin):
+    list_display = ('body', 'date')
+
+
+admin.site.register(UserQuery, UserQueryAdmin)
--- index/models.py
+++ index/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserQuery(models.Model):
+    body = models.CharField(max_length = 40)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
